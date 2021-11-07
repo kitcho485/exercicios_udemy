7ac0e6a497c047c69df719e721491517f7ea8ddc
Claudio Nascimento dos Santos <93013684+kitcho485@users.noreply.github.com>
2021-11-07 16:29:53 -0300

    Add files via upload

---
+++ 1009 Salario com bonus/SalarioBonus/SalarioBonus/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+
+namespace SalarioBonus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome;
+            double salario, totalvendas, salarioComissao;
+
+            nome = Console.ReadLine();
+            salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            totalvendas = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            salarioComissao = totalvendas * 15.0 / 100 + salario;
+
+            Console.WriteLine("TOTAL = R$ " + salarioComissao.ToString("F2", CultureInfo.InvariantCulture));
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 1010 Calculo Simples/CalculoSimples/CalculoSimples/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+
+namespace CalculoSimples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int codPeca1, codPeca2, qtdaPeca1, qtdaPeca2;
+            double preco1, preco2, total;
+
+            string[] valores = Console.ReadLine().Split(' ');
+
+            codPeca1 = int.Parse(valores[0]);
+            qtdaPeca1 = int.Parse(valores[1]);
+            preco1 = double.Parse(valores[2], CultureInfo.InvariantCulture);
+
+            valores = Console.ReadLine().Split(' ');
+            codPeca2 = int.Parse(valores[0]);
+            qtdaPeca2 = int.Parse(valores[1]);
+            preco2 = double.Parse(valores[2], CultureInfo.InvariantCulture);
+
+            total = preco1 * qtdaPeca1 + preco2 * qtdaPeca2;
+
+            Console.WriteLine("VALOR A PAGAR: R$ " + total.ToString("F2", CultureInfo.InvariantCulture));
+            Console.ReadLine();
+
+        }
+    }
+}
