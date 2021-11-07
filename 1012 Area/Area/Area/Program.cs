f749aeb68b1fd4f5b9cdc215b53725faa1745b52
Claudio Nascimento dos Santos <93013684+kitcho485@users.noreply.github.com>
2021-11-07 16:32:23 -0300

    Add files via upload

---
+++ 1012 Area/Area/Area/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+
+namespace Area
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, c, trianguloRetangulo, circulo, trapezio, quadrado, retangulo;
+
+            String[] valores = Console.ReadLine().Split(' ');
+            a = double.Parse(valores[0], CultureInfo.InvariantCulture);
+            b = double.Parse(valores[1], CultureInfo.InvariantCulture);
+            c = double.Parse(valores[2], CultureInfo.InvariantCulture);
+ 
+            trianguloRetangulo = a * c / 2.0;
+            circulo = 3.14159 * c * c;
+            trapezio = (a + b) / 2.0 * c;
+            quadrado = b * b;
+            retangulo = a * b;
+
+            Console.WriteLine("Triangulo: " + trianguloRetangulo.ToString("F3", CultureInfo.InvariantCulture));
+            Console.WriteLine("Ciruculo: " + circulo.ToString("F3", CultureInfo.InvariantCulture));
+            Console.WriteLine("Trapezio: " + trapezio.ToString("F3", CultureInfo.InvariantCulture));
+            Console.WriteLine("Quadrado: " + quadrado.ToString("F3", CultureInfo.InvariantCulture));
+            Console.WriteLine("Retangulo: " + retangulo.ToString("F3", CultureInfo.InvariantCulture));
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ 1014 Consumo/Consumo/Consumo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+namespace Consumo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int distancia;
+            double qtdaLitro, combustivelGasto;
+
+            distancia = int.Parse(Console.ReadLine());
+            qtdaLitro = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            combustivelGasto = distancia / qtdaLitro;
+
+            Console.WriteLine(combustivelGasto.ToString("F3", CultureInfo.InvariantCulture) + " km/l");
+            Console.ReadLine();
+        }
+    }
+}
