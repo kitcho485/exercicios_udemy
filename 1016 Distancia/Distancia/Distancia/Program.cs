d1cb9cc70bc2123d7daaad22f1cac5ad965c3dbb
Claudio Nascimento dos Santos <93013684+kitcho485@users.noreply.github.com>
2021-11-07 16:34:14 -0300

    Add files via upload

---
+++ 1016 Distancia/Distancia/Distancia/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Distancia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int tempo, distancia;
+
+            distancia = int.Parse(Console.ReadLine());
+
+            tempo = distancia * 2;
+
+            Console.WriteLine(tempo + " minutos");
+            Console.ReadLine();
+        }
+    }
+}
