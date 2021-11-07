c7016b724491198a8642b80b2380b4b47974515a
Claudio Nascimento dos Santos <93013684+kitcho485@users.noreply.github.com>
2021-11-07 16:13:48 -0300

    Add files via upload

---
+++ Calculo de area de um terreno/Calculo de area de terreno/Calculo de area de terreno/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+
+namespace Calculo_de_area_de_terreno
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double largura, comprimento, precoMetroQadrado, area, preco;
+
+            largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            comprimento = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            precoMetroQadrado = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            area = comprimento * largura;
+            preco = area * precoMetroQadrado;
+
+            Console.WriteLine("Area = " + area.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Preço = " + preco.ToString("F2", CultureInfo.InvariantCulture));
+
+        }
+    }
+}
