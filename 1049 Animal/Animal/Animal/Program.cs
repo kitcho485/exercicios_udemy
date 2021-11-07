606c836a59709105a83270e1b2796cc32d2ee364
Claudio Nascimento dos Santos <93013684+kitcho485@users.noreply.github.com>
2021-11-07 16:40:37 -0300

    Add files via upload

---
+++ 1049 Animal/Animal/Animal/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Animal {
+    class Program {
+        static void Main(string[] args) {
+            string palavra1 = Console.ReadLine();
+            string palavra2 = Console.ReadLine();
+            string palavra3 = Console.ReadLine();
+
+            if (palavra1 == "vertebrado") {
+                if (palavra2 == "ave") {
+                    if (palavra3 == "carnivoro") {
+                        Console.WriteLine("aguia");
+                    } else {
+                        Console.WriteLine("pomba");
+                    }
+                }
+            }
+        }
+    }
+}
