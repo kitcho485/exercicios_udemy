cf04667e89f11974b16436aa55f8035b7aaaef41
Claudio Nascimento dos Santos <93013684+kitcho485@users.noreply.github.com>
2021-11-07 16:15:13 -0300

    Add files via upload

---
+++ Contagem Regressiva(Estrutura FOR)/ContagemRegressiva/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace ContagemRegressiva {
+    class Program {
+        static void Main(string[] args) {
+            for (int i=15; i>0; i--) {
+                Console.WriteLine("valor de i: " + i);
+            }
+        }
+    }
+}
---
+++ Foreach/Foreach/Foreach/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Foreach {
+    class Program {
+        static void Main(string[] args) {
+
+            int N = 0;
+            N = int.Parse(Console.ReadLine());
+            String[] vet = new string[N];
+
+            for (int i = 0; i<N; i++) {
+                vet[i] = Console.ReadLine();
+            }
+            Console.WriteLine("Nome lidos");
+            // Para se usar o foreach é necessario dar um apelido para o foreach no casa nomesLidos
+            foreach (string nomesLidos in vet) {
+                Console.WriteLine(nomesLidos);
+            }
+        }
+    }
+}
