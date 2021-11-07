8e4795dfed318c4cb0f8443e69b1206fdd59b1cb
Claudio Nascimento dos Santos <93013684+kitcho485@users.noreply.github.com>
2021-11-07 16:38:39 -0300

    Add files via upload

---
+++ 1041 Coordenadas de um ponto/CoordenadasDeUmPonto/CoordenadasDeUmPonto/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Globalization;
+
+namespace CoordenadasDeUmPonto {
+    class Program {
+        static void Main(string[] args) {
+
+            string[] vet = Console.ReadLine().Split(' ');
+
+            double x = double.Parse(vet[0],CultureInfo.InvariantCulture);
+            double y = double.Parse(vet[1], CultureInfo.InvariantCulture);
+
+            if (x == 0.0 && y == 0.0){
+                Console.WriteLine("Origem");
+            } 
+            else if (x == 0.0) {
+                Console.WriteLine("Eixo y");
+            } 
+            else if (y == 0.0) {
+                Console.WriteLine("Eixo x");
+            } 
+            else if (x > 0.0 && y > 0.0) {
+                Console.WriteLine("Q1");
+            } 
+            else if (x < 0.0 && y > 0.0) {
+                Console.WriteLine("Q2");
+            } 
+            else if (x < 0.0 && y < 0.0) {
+                Console.WriteLine("Q3");
+            } 
+            else {
+                Console.WriteLine("Q4");
+            }
+        }
+    }
+}
